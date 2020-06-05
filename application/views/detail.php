91b2ec4de384810f512575abd49749f0accb548e
hendraaaa <44520819+hendraaaa@users.noreply.github.com>
2020-06-05 13:37:23 +0800

    p

--- application/views/detail.php
+++ application/views/detail.php
@@ -28,7 +28,7 @@
 <body>
 
 	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-		  <a class="navbar-brand" href="#">Navbar</a>
+		  <a class="navbar-brand" href="#">Navbarr</a>
 		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 		    <span class="navbar-toggler-icon"></span>
 		  </button>
@@ -80,7 +80,7 @@
 		</nav>
 
 		
-	<div class="jumbotron">
+	<div class="bg-img">
 		<h4>hello world</h4>
 	</div>
 	
