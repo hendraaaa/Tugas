851de6741ccd37b606c544d6679de4510cf27f33
shabri53 <66395992+shabri53@users.noreply.github.com>
2020-06-19 19:04:11 +0800

    tes

--- application/views/home.php
+++ application/views/home.php
@@ -42,7 +42,7 @@
 		          Movie
 		        </a>
 		        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-		          <a class="dropdown-item" href="application\views\favorite.php">Action</a>
+		          <a class="dropdown-item" href="#">Action</a>
 		          <a class="dropdown-item" href="#">Another action</a>
 		          <div class="dropdown-divider"></div>
 		          <a class="dropdown-item" href="#">Something else here</a>
